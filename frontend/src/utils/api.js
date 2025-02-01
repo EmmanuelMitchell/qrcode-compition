11c79f182955d53ee1606fb764be68e954898a56
EmmanuelMitchell <emmanuelmitchell1998@gmail.com>
2025-02-01 19:59:42 +0000

    change vit import dotenv

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -56,7 +56,10 @@
 
 
 // const API_BASE_URL = 'http://localhost:3001/api';
-const API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/api`;
+// const API_BASE_URL = `${import.meta.env.process.env.REACT_APP_BACKEND_URL}/api`;
+const API_BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
+console.log(API_BASE_URL);
+
 
 
 
